7cc025082a6ec4d9febbb8135800f47c92d278cd
Mathijs Groothuis <mathijs.groothuis@gmail.com>
2019-10-16 16:02:25 +0200

    Working logic \0/

--- javascript/galgje.js
+++ javascript/galgje.js
@@ -1,107 +1,120 @@
 
 // Woorden-array
 var wordList = ["Bananendoos", "Theelepel", "Herbivoor", "Kaassoufle", "Sjaggeraar", "Komkommerbedrijf", "Telecommunicatiewetgeving", "Fluviatiel", "Coniferenhaag"];
-
-// Kies een random woord uit de array
+// Kies een random woord uit de array en zet deze naar geheel lowercase
 let word = wordList[Math.floor(Math.random() * wordList.length)].toLowerCase();
-console.log("Het woord is: " + word);
+console.log("Het spelwoord is: '" + word + "'");
+
+// Hoeveel keer je een letter fout mag hebben
 var amountOfTries = 5;
 
-var indexOfCorrectLetters = []; // Array met geraden letters
-//submitButton.addEventListener("click", function(){ checkLetter();});
+// Set object met letters. Bevat unieke letters
+var guessedLetters = new Set();
 
-var guessedLetter = [""]; // Array met geraden letters
+
+// DOM koppeling
+var counterElement = document.querySelector("#counter");
+var letterInputElement = document.querySelector("#letterInput");
+let letterOutputElement = document.querySelector("#letterOutput"); // Div waar de letters worden gerenderd
+let submitButton = document.querySelector("#submit"); // Submit-knop
+
+
+var letterInput = letterInputElement.value;
+// Wanneer de input veranderd van het letterveld draai de submitcheck
+letterInputElement.oninput = submitCheck;
+
+renderLetters(word);
 
 // Koppel eventlisteners als DOM geladen is aan bewuste elementen
-window.onload = function ()
-{
-    let letterOutput = document.querySelector("#letterOutput"); // Div waar de letters worden gerenderd
-    var submitButton = document.querySelector("#submit"); // Submit-knop
-    letterInput = document.querySelector("#letterInput");
-    letterInput.addEventListener('change', function () { enableSubmit(this.value, submitButton); });
-    submitButton.disabled = false;
-    renderLetters(word);
-    //submitButton.addEventListener("click", function(){ checkLetter();});
-    //var submittedLetter = document.querySelector("#letterInput").value;
-    //checkLetter(submittedLetter);
-}
 
-function enableSubmit(input, button)
+// Functie die submitknop enabled en disabled
+function submitCheck()
 {
-    if (input == "")
+    var letterInput = letterInputElement.value;
+    // Wanneer er geen letter is ingegeven
+    if (letterInput == "")
     {
-        button.disabled = true;
+        // Disable submiknop
+        submitButton.disabled = true;
     }
+    // Wanneer er wel een letter is ingegeven
     else
     {
-        button.disabled = false;
+        // Schakel submitknop in
+        submitButton.disabled = false;
     }
 }
 
-function renderLetters(word, guessedLetter)
+function renderLetters(word, letter)
 {
-    let content = "";
+    let content = document.createTextNode("");
 
-    // Itereer door alle letters van het woord
+    // Als er eerder geraden is, en dus het woord al eens is gerenderd, haal het woord dan weg.
+    if (arguments.length > 1)
+    {
+        // Haal de div leeg
+        letterOutputElement.innerHTML = "";
+    }
+
+
+    // Itereer door alle letters van het spelwoord
     for (let i = 0; i < word.length; i++)
     {
+        // Maak in elk geval per letter een div aan
         let element = document.createElement("div");
 
-        // Als gekozen letter in het woord valt, render dan de letter
-        if (word[i].includes("e"))
+        // Als er een geraden letter in de set zit.
+        if (guessedLetters.size > 0)
         {
-            content = document.createTextNode(word[i]);
+            // Als de bewuste letter van het woord in de geraden letters zit
+            if (guessedLetters.has(word[i]))
+            {
+                // Render dan de letter in plaats van een streepje
+                content = document.createTextNode(word[i]);
+            }
+             // Zo niet, render dan een streepje
+             else
+             {
+                 content = document.createTextNode("_");
+             }
         }
-        // Zo niet, render dan een streepje
-        else
-        {
-            content = document.createTextNode("_");
-        }
-
+         // Als er geen geraden letter in de set zit, render dan sowieso een streepje
+         else
+         {
+             content = document.createTextNode("_");
+         }
         element.appendChild(content);
-        element.classList.add("letter", "p-3", "m-1", "badge", "badge-secondary");
-        letterOutput.appendChild(element);
-
-        // // Als er juiste woorden zijn geraden
-        // if (indexOfCorrectLetters.length > 0)
-        // {
-        //     for (i=0; i < indexOfCorrectLetters.length; i++)
-        //     {
-        //         if (indexOfCorrectLetters[i].value == i)
-        //         {
-        //             console.log("De check conditie klopt");
-        //             let content = document.createTextNode(word.charAt(i));
-        //         }
-        //     }
-        //     content = document.createTextNode(word.charAt(i));
-        // }
-        // else
-        // {
-        //     content = document.createTextNode("_");
-        // }
+        element.classList.add("letter", "p-3", "m-1", "badge", "badge-secondary", "shadow");
+        letterOutputElement.appendChild(element);
     }
 }
 
 function checkLetter()
 {
-    guessedLetter.push();
-    console.log("Door de speler gekozen letter is: " + guessedLetter);
-    var letterArray = [];
-    var count = 0;
-    for (i = 0; i < word.length; i++)
-    {
-        console.log("De waarde van de letter op plek " + i + " is: " + word.charAt(i));
-        if (guessedLetter == word.charAt(i))
-        {
-            indexOfCorrectLetters.push(i);
-            count = count + 1;
-        }
-        console.log(word[i]);
-    }
-    console.log("De ingevoerde letter '" + guessedLetter + "' komt " + count + " keer voor in " + word);
-    console.log("De geraden letters staan op plek: " + indexOfCorrectLetters);
-    //console.log("Indexgrootte: " + indexOfCorrectLetters.length);
-    renderLetters(word, guessedLetter);
+    letterInput = letterInputElement.value.toLowerCase();
 
-    console.log(guessedLetter);
+    // Check of de ingevoerde letter voor komt in het woord
+    if (word.includes(letterInput) == true)
+    {
+        // voeg huidige gekozen letter toe aan set van geraden letters
+        guessedLetters.add(letterInput);
+    }
+    else
+    {
+        // Zo niet, gaat er een punt af van de hoeveelheid beurten
+        if (amountOfTries > 0)
+        {
+            amountOfTries--;
+        }
+        else
+        {
+            alert("DOOD!");
+        }
+
+        counterElement.innerHTML = amountOfTries;
+    }
+    renderLetters(word, letterInput);
+
+    letterInputElement.value = "";
+    //console.log("word: " + word + " letterInput: " + letterInput);
 }
